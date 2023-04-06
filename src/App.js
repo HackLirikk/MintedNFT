5082d5e090ecdcf19b0c00a6275511826a023937
Kirill Ruchkin <lirikk@MacBook-Pro-Kirill.local>
2023-04-06 19:32:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,186 @@
+import { useState, useEffect } from 'react'
+import { ethers } from 'ethers'
+import Modal from './components/Modal'
+import openseaLogo from './assets/opensea-logo.png'
+import NFT from './utils/NFT.json'
+import './App.css';
+
+
+const OPENSEA_LINK = 'https://testnets.opensea.io/collection/monkey-42';
+const TOTAL_MINT_COUNT = 50;
+
+const CONTRACT_ADDRESS = "0xFf44D48515cD41F633DcAC9F22a665efbC4631f3";
+
+const App = () => {
+  let totalMinted
+  const [currentAccount, setCurrentAccount] = useState("")
+  const [miningAnimation, setMiningAnimation] = useState(false)
+  const [mintTotal, setMintTotal] = useState(totalMinted)
+  const [currentNetwork, setCurrentNetwork] = useState("")
+
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log("Make sure you have metamask!")
+      return;
+    } else {
+      console.log("We have the ethereum object", ethereum);
+      console.log(window.ethereum.networkVersion, 'window.ethereum.networkVersion');
+    }
+    
+    const accounts = await ethereum.request({ method: 'eth_accounts' });
+    
+    if (accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account)
+      //setupEventListener()
+    } else {
+      console.log("No authorized account found")
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("Get Metamask!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Connected", accounts[0])
+      setCurrentAccount(accounts[0])
+
+      //setupEventListener()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const setupEventListener = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, NFT.abi, signer);
+        //console.log(connectedContract);
+
+        connectedContract.on("Monkey", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`Hey! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
+        })
+        console.log("Setup event listener!")
+      } else {
+        console.log("Ethereum object doesn't exist")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const askContractToMintNft = async () => {
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const provider = new ethers.providers.Web3Provider(ethereum);
+          const signer = provider.getSigner()
+          const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, NFT.abi, signer)
+          let accounts = await provider.send("eth_requestAccounts", []);
+          let account = accounts[0];
+          console.log(account)
+
+          console.log("Going to pop wallet now to pay gas...")
+          let nftTxn = await connectedContract.safeMint(account, {
+            value: ethers.utils.parseEther("0.001"),
+          })
+
+          console.log("Mining... please wait")
+          setMiningAnimation(true);
+          await nftTxn.wait()
+          console.log(nftTxn)
+          console.log(`Mined, tee transaction: https://testnet.bscscan.com/tx/${nftTxn.hash}`)
+          setMiningAnimation(false)
+        } else {
+          console.log("Ethereum object doesn't exist")
+        }
+      } catch (error) {
+        console.log(error)
+      }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected()
+    //getTotalNFTsMintedSoFar()
+  }, [])
+
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      Connect to Wallet
+    </button>
+  );
+
+  const renderMintUI = () => (
+    <button onClick={askContractToMintNft} className="cta-button mint-button">
+      Mint NFT
+    </button>
+  )
+
+  const renderNetworkPrompt = () => (
+    alert("Hello there, This app is built on the bnb testnet and it looks like you are on a different ethereum network. Please switch to the Bscscan testnet to continue")
+  )
+
+  const getTotalNFTsMintedSoFar = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, NFT.abi, signer);
+    
+    let count = await connectedContract.getTotalNFTsMintedSoFar()
+    setMintTotal(count._hex.substring(3))
+    console.log(count._hex.substring(3))
+  }
+
+  return (
+    <div className="App">
+      {
+        miningAnimation ? (
+          <Modal />
+        ) : null
+      }
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">Monkey NFT Collection</p>
+          <a
+            className="opensea-button"
+            href={OPENSEA_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={openseaLogo} alt="opensea-logo" className="opensea-logo" />View Collection on OpenSea</a>
+          <p className="sub-text">
+            Each unique. Each inspiring.
+          </p>
+          {/* <p className="sub-text">
+            {mintTotal}/50 NFTs minted. Discover your NFT today.
+          </p> */}
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
+        </div>
+      </div>
+      <div className="circle1"></div>
+      <div className="circle2"></div>
+      <div className="wave wave1"></div>
+      <div className="wave wave2"></div>
+      {/* put little guy on wave3 */}
+      <div className="wave wave3"></div>
+      <div className="wave wave4"></div>
+    </div>
+  );
+}
+
+export default App;
